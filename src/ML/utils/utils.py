63ce3608144d7602517350663b0b8fde5f66a444
anpirott <antoine.pirotte@ulb.be>
2025-11-30 17:24:49 +0100

    improved the comparison function to be able to see all the results without having to manually write them

--- src/ML/utils/utils.py
+++ src/ML/utils/utils.py
@@ -69,16 +69,16 @@ def print_uniques_count(col_name, df):
     for unique in uniques:
         print(f"\t{unique} => {np.count_nonzero(df[col_name] == unique)}")
 
-def compare_metrics(path : str, model_names : list[str], physical_models : list[str], data_filters : list[str], output_parameters : list[str]) -> pd.DataFrame:
+def compare_metrics(path : str, output_parameters : list[str], model_names : list[str]=None, physical_models : list[str]=None, data_filters : list[str]=None) -> pd.DataFrame:
     """
     Compares the statistics between one or more models in a single table and displays it. # TODO? aussi le return?
     The path to the metrics and images needs to follow this hierarchy : path/to/results/(training_type/)model_name/physical_model/data_filter/
 
     Parameters:
         path (str) : path to the results folder containing the models
-        model_names list(str) : name of the models which need to be added to the comparison
-        physical_models list(str) : which physical model ("MIST" and "PARSEC") needs to be used in the comparison
-        data_filters list(str) : which filters need to be used in the comparison
+        model_names list(str) : name of the models which need to be added to the comparison; if set to None, uses all possible values in the directory
+        physical_models list(str) : which physical model ("MIST" and "PARSEC") needs to be used in the comparison; if set to None, uses all possible values in the directory
+        data_filters list(str) : which filters need to be used in the comparison; if set to None, uses all possible values in the directory
         output_parameters list(str) : which output parameters of the model we want to add to the comparison
     
     the dataframe has the following form:
@@ -101,8 +101,36 @@ def compare_metrics(path : str, model_names : list[str], physical_models : list[
     ...
     ...
     """
-
     path = sanitize_path(path)
+
+    if model_names is None: # TODO? je peux faire une fct de ça mais pas du tout obligatoire
+        model_names = []
+        for file_or_directory in os.listdir(path):
+            if os.path.isdir(path + f"{file_or_directory}"):
+                model_names.append(file_or_directory)
+    if len(model_names) == 0:
+        print("Error : no model with existing results.")
+        sys.exit(1)
+
+    if physical_models is None:
+        physical_models = []
+        new_path = path + f"{model_names[0]}/"
+        for file_or_directory in os.listdir(new_path):
+            if os.path.isdir(new_path + f"{file_or_directory}"):
+                physical_models.append(file_or_directory)
+    if len(physical_models) == 0:
+        print("Error : no results for any physical model exists.")
+        sys.exit(1)
+
+    if data_filters is None:
+        data_filters = []
+        new_path = path + f"{model_names[0]}/{physical_models[0]}/"
+        for file_or_directory in os.listdir(new_path):
+            if os.path.isdir(new_path + f"{file_or_directory}"):
+                data_filters.append(file_or_directory)
+    if len(data_filters) == 0:
+        print("Error : no results for any physical model exists.")
+        sys.exit(1)
 
     results_dict = dict()
     for model_name in model_names:
@@ -115,12 +143,11 @@ def compare_metrics(path : str, model_names : list[str], physical_models : list[
 
                 with open(full_path + "metrics.csv", 'r') as metrics_file:
                     with open(full_path + "time_taken.txt", 'r') as time_file:
-                        lines = time_file.readlines()
-                        dict_reader = csv.DictReader(metrics_file)
+                        lines = time_file.readlines() # retrieving the time taken
+                        dict_reader = csv.DictReader(metrics_file) # retrieving the metrics
 
                         for metrics_dict in list(dict_reader):
-                            output_parameter = metrics_dict.pop("")
-
+                            # creating a nicer way to display the percentiles
                             percentiles = eval(metrics_dict.pop("Percentiles"))
                             str_percentiles = ""
                             for thresh, value in percentiles.items():
@@ -128,11 +155,14 @@ def compare_metrics(path : str, model_names : list[str], physical_models : list[
                             str_percentiles = str_percentiles[:-1]
                             metrics_dict["Percentiles"] = str_percentiles
 
-                            metrics_dict["time"] = round(float(lines[1].split(',')[0]), 5)
+                            metrics_dict["time"] = round(float(lines[1].split(',')[0]), 5) # adding the time to the metrics dictionnary
 
-                            if output_parameter in output_parameters:
+                            output_parameter = metrics_dict.pop("")
+
+                            if output_parameter in output_parameters: # adding the metrics dictionnary
                                 results_dict[model_name][physical_model][data_filter][output_parameter] = copy.deepcopy(metrics_dict)
     
+    # creating the dataframe as is shown in the docstring
     rows = []
     for model_name, physical_model_dict in results_dict.items():
         for physical_model, data_filter_dict in physical_model_dict.items():
@@ -141,8 +171,8 @@ def compare_metrics(path : str, model_names : list[str], physical_models : list[
                     row = {"model": model_name, "physical_model": physical_model, "filter": data_filter, "output_parameter": output_parameter}
                     row.update(metrics_dict)
                     rows.append(row)
-    
     df = pd.DataFrame(rows).set_index(["model", "physical_model", "filter", "output_parameter"])
+
     pd.set_option('display.max_colwidth', None)
     display(df)
 
